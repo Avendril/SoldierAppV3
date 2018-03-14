18e3f5bf07cd589eeff91d951291cc4e7af9573f
Bartosz Chmielewski <nexerio7991@gmail.com>
2018-03-14 11:57:30 +0000

    Insert is working
    Insert method in php is working with the java code in BackgroundWorker to utilise it.

--- app/src/main/java/com/example/nexer/myapplication/BackgroundWorker.java
+++ app/src/main/java/com/example/nexer/myapplication/BackgroundWorker.java
@@ -31,10 +31,12 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
     protected String doInBackground(String... params) {
         String type = params[0];
         String login_url = "http://10.0.2.2/login.php"; //If not working, use the ip that DNS gave you ipconfig
+        String insert_url = "http://10.0.2.2/insert.php";
         if(type.equals("login")) {
             try {
                 String username = params[1];
                 String password = params[2];
+
                 URL url = new URL(login_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setRequestMethod("POST"); //Type of data that PHP files (conn.php and Login.php) has to use
@@ -42,8 +44,10 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
                 String post_data = URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"
-                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8"); //using the username and password above to check for inside the db
+                                  +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8"); //using the username and password above to check for inside the db
+
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
                 bufferedWriter.close();
@@ -52,12 +56,67 @@ public class BackgroundWorker extends AsyncTask<String,Void,String> {
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
                 String result="";
                 String line="";
+
                 while((line = bufferedReader.readLine())!= null) {
                     result += line;
                 }
                 bufferedReader.close();
                 inputStream.close();//Close both buffer and reader, not flushing since this might be needed later
                 httpURLConnection.disconnect();//Disconnecting from  the database after the login is finished!
+
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(type.equals("insert")){
+            try {
+                String name = params[1];
+                String surname = params[2];
+                String email = params[3];
+                String phoneNo = params[4];
+                String address1 = params[5];
+                String address2 = params[6];
+                String rank = params[7];
+                String username = params[8];
+                String password = params[9];
+
+                URL url = new URL(insert_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST"); //Type of data that PHP files (conn.php and Login.php) has to use
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String post_data = URLEncoder.encode("name","UTF-8")+"="+URLEncoder.encode(name,"UTF-8")+"&"
+                                  +URLEncoder.encode("surname","UTF-8")+"="+URLEncoder.encode(surname,"UTF-8")+"&"
+                                  +URLEncoder.encode("email","UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"
+                                  +URLEncoder.encode("phoneNo","UTF-8")+"="+URLEncoder.encode(phoneNo,"UTF-8")+"&"
+                                  +URLEncoder.encode("address1","UTF-8")+"="+URLEncoder.encode(address1,"UTF-8")+"&"
+                                  +URLEncoder.encode("address2","UTF-8")+"="+URLEncoder.encode(address2,"UTF-8")+"&"
+                                  +URLEncoder.encode("militaryRank","UTF-8")+"="+URLEncoder.encode(rank,"UTF-8")+"&"
+                                  +URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"
+                                  +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8"); //using the username and password above to check for inside the db
+
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();//Flushing the buffer so it doesn't use so much memory and closing it afterwards just to save some memory
+                InputStream inputStream = httpURLConnection.getInputStream();//open another inputstream
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+
+                bufferedReader.close();
+                inputStream.close();//Close both buffer and reader, not flushing since this might be needed later
+                httpURLConnection.disconnect();//Disconnecting from  the database after the login is finished!
+
                 return result;
             } catch (MalformedURLException e) {
                 e.printStackTrace();
--- app/src/main/java/com/example/nexer/myapplication/InsertActivity.java
+++ app/src/main/java/com/example/nexer/myapplication/InsertActivity.java
@@ -2,12 +2,43 @@ package com.example.nexer.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class InsertActivity extends AppCompatActivity {
+
+    EditText name, surname, email, phone, address1, address2, militaryRank, username, password;
+    String str_name, str_surname, str_email, str_phone, str_address1, str_address2, str_militaryRank, str_username, str_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        name = (EditText)findViewById(R.id.insertName);
+        surname = (EditText)findViewById(R.id.insertSurname);
+        email = (EditText)findViewById(R.id.insertEmail);
+        phone = (EditText)findViewById(R.id.insertPhoneNo);
+        address1 = (EditText)findViewById(R.id.insertAddress1);
+        address2 = (EditText)findViewById(R.id.insertAddress2);
+        militaryRank = (EditText)findViewById(R.id.insertRank);
+        username = (EditText)findViewById(R.id.insertUsername);
+        password = (EditText)findViewById(R.id.insertPassword);
+    }
+
+    public void OnRegister(View view){
+        str_name = name.getText().toString();
+        str_surname = surname.getText().toString();
+        str_email = email.getText().toString();
+        str_phone = phone.getText().toString();
+        str_address1 = address1.getText().toString();
+        str_address2 = address2.getText().toString();
+        str_militaryRank = militaryRank.getText().toString();
+        str_username = username.getText().toString();
+        str_password = password.getText().toString();
+
+        String type = "insert";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_name, str_surname, str_email, str_phone, str_address1, str_address2, str_militaryRank, str_username, str_password);
     }
 }
